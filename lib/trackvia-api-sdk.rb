a3c021dc029adcffe525f10b663271161dca64da
Andrew Cottage <andcott@gmail.com>
2017-10-04 14:32:26 -0400

    - one more typo :/

--- lib/trackvia-api-sdk.rb
+++ lib/trackvia-api-sdk.rb
@@ -428,7 +428,7 @@ module Trackvia
         url = "#{base_uri}/openapi/views/#{view_id}/records?#{encoded_auth_url}"
         new_records = { "data" => batch }
         json = RestClient.post url, new_records.to_json, { :accept => :json, :content_type => :json }
-        record = JSON.parse(json)
+        records = JSON.parse(json)
       rescue RestClient::Exception => e
         retry if maybe_retry_when_bad_auth_token(e)
       end
